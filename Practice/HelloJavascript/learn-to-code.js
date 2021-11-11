9a3d3963641c3e43c67b6a832e10a413bf411583
Anupa <thapaanupa@gmail.com>
2021-11-11 09:13:27 +0200

    new commit

---
+++ Practice/HelloJavascript/learn-to-code.js
@@ -0,0 +1,15 @@
+//var name = "Anupa";
+//var age = 34;
+//var shootingScore = 45.6;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+var firstName = "John";
+var lastname = "JacobJingleHeimerSmith";
+var dateOfBirth = "10-09-82";
+var age = "38";
+var profileImage = ""
+
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday! ";
+
+console.log(loginWelcomeMessage);
